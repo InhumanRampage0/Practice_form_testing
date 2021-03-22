99a722fe47608f4da79f7a638785f192f9a53320
InhumanRampage0 <Boss4703264@gmail.com>
2021-03-22 16:40:11 +0300

    HomeWork #2 rework

--- src/test/java/BoxPracticeForm.java
+++ src/test/java/BoxPracticeForm.java
@@ -8,14 +8,15 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class BoxPracticeForm {
-    @Test
+@Test
     void selenideSearchTest() {
         open("https://demoqa.com/automation-practice-form");
 // usersData
         $("#firstName").setValue("Bogdan");
         $("#lastName").setValue("Pershko");
         $("#userEmail").setValue("Boduk99@mail.ru");
-        $("#gender-radio-1").doubleClick();
+        $(byText("Male")).click();
+       // $("#gender-radio-1").doubleClick(); - как вариант.
         $("#userNumber").setValue("0123456789");
 // dataPicker
         $("#dateOfBirthInput").click();
@@ -23,7 +24,6 @@ public class BoxPracticeForm {
         $(".react-datepicker__year-select").selectOptionByValue("1992");
         $(".react-datepicker__day--028").click();
 // subjects
-        $("#subjectsContainer").click();
         $("#subjectsInput").setValue("English").pressEnter();
         $("#subjectsInput").setValue("Hindi").pressEnter();
         $("#subjectsInput").setValue("Commerce").pressEnter();
@@ -39,7 +39,7 @@ public class BoxPracticeForm {
         $("#react-select-4-input").setValue("Noida").pressEnter();
         $("#submit").click();
 
-        $(".modal-content").shouldHave(text("Boduk99@mail.ru"),
+        $(".modal-content").shouldHave(text("Boduk99@mail.ru"), text("Bogdan Pershko"),
              text("0123456789"), text("28 April,1992"), text("English, Hindi, Commerce"),
                 text("Sports, Music"), text("Sevastopol"), text("NCR Noida"), text("kumamon.jpg"));
 
